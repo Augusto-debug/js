e0ce47596862afdad5cc1649e9bd64e017d8cc54
Augusto <augustocesarrezende64@gmail.com>
2024-10-24 21:25:29 -0300

    codigo modularizado

--- aula05-dia24-10-24/listaExercicios/ex08/index.js
+++ aula05-dia24-10-24/listaExercicios/ex08/index.js
@@ -3,10 +3,20 @@ const variados = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ,0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
+let tamanhoSenha = parseInt(prompt('Digite o tamanho da senha: '));
 let resultado = [];
-for(let i = 0; i < 8; i++){
+for(let i = 0; i < tamanhoSenha; i++){
    resultado.push(variados[Math.floor(Math.random() * variados.length)]);
 }
 
-console.log(resultado.join(''));
+document.write(resultado.join(''));
+
+const generatePassword = (tamanhoSenha) => {
+   let resultado = [];
+   for(let i = 0; i < tamanhoSenha; i++){
+      resultado.push(variados[Math.floor(Math.random() * variados.length)]);
+   }
+   return resultado.join('');
+};
+
+console.log(generatePassword(10)); // 10 é o tamanho da senha
